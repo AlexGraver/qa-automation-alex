439460cdaf56ac2b1d0300aa379cba4e478097a9
AlexGraver <125137684+AlexGraver@users.noreply.github.com>
2023-03-04 15:55:37 +0200

    Delete Trip.java

--- src/test/java/hw1/Trip.java
+++
@@ -1,9 +0,0 @@
-package hw1;
-
-public class Trip {
-    public double getLitres(Vehicle) {
-        double litres = (Rout.getDistance()/100)*Vehicle.getAverageFuelConsumption();
-        return litres;
-        System.out.println(litres);
-    }
-}
